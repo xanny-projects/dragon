d8188a0c7e85797cd17afcd25414d6ce7b956c60
yasser <spookyframework@gmail.com>
2020-12-05 15:33:06 +0100

    feat(lib): Gets the response status code.

---
+++ lib/httpResponse.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { assert, Response } from "../deps.ts";
+import { HttpMessage } from "./httpMessage.ts";
+import { HttpStatus } from "./httpError.ts";
+
+/**
+ * Representation of an outgoing, server-side response.
+ *
+ * Per the HTTP specification, this interface includes properties for
+ * each of the following:
+ *
+ * - Status code
+ * - Redirect
+ * - Header
+ *
+ */
+export class HttpResponse extends HttpMessage {
+
+  /**
+   * Construct a new, empty instance of the {@code HttpResponse} object.
+   * @param {Response} res
+   */
+  constructor(private readonly res: Response) {
+    super(res.headers || new Headers());
+  }
+
+  /**
+   * Gets the response status code.
+   * The status code is a 3-digit integer result code of the server's attempt
+   * to understand and satisfy the request.
+   *
+   * @returns {number}
+   * @api public
+   */
+  public GetStatusCode(): number {
+    return this.res.status || HttpStatus.OK;
+  }
+
+}

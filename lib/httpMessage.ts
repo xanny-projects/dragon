edd9ebe40e69e31eeed9414d197847c712951113
yasser <spookyframework@gmail.com>
2020-12-11 23:40:55 +0100

    feat(lib): Add List of HTTP headers

--- lib/httpMessage.ts
+++ lib/httpMessage.ts
@@ -17,6 +17,56 @@
 import { HttpError } from "./httpError.ts";
 
 /**
+ * HTTP headers let the client and the server pass additional information with an HTTP request or response.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers}
+ */
+export enum Header {
+  AcceptEncoding = "Accept-Encoding",
+  Allow = "Allow",
+  Authorization = "Authorization",
+  ContentDisposition = "Content-Disposition",
+  ContentEncoding = "Content-Encoding",
+  ContentLength = "Content-Length",
+  ContentType = "Content-Type",
+  Cookie = "Cookie",
+  SetCookie = "Set-Cookie",
+  IfModifiedSince = "If-Modified-Since",
+  LastModified = "Last-Modified",
+  Location = "Location",
+  Upgrade = "Upgrade",
+  Vary = "Vary",
+  WWWAuthenticate = "WWW-Authenticate",
+  XForwardedFor = "X-Forwarded-For",
+  XForwardedProto = "X-Forwarded-Proto",
+  XForwardedProtocol = "X-Forwarded-Protocol",
+  XForwardedSsl = "X-Forwarded-Ssl",
+  XUrlScheme = "X-Url-Scheme",
+  XHTTPMethodOverride = "X-HTTP-Method-Override",
+  XRealIP = "X-Real-IP",
+  XRequestID = "X-Request-ID",
+  XRequestedWith = "X-Requested-With",
+  Server = "Server",
+  Origin = "Origin", // Access control
+  AccessControlRequestMethod = "Access-Control-Request-Method",
+  AccessControlRequestHeaders = "Access-Control-Request-Headers",
+  AccessControlAllowOrigin = "Access-Control-Allow-Origin",
+  AccessControlAllowMethods = "Access-Control-Allow-Methods",
+  AccessControlAllowHeaders = "Access-Control-Allow-Headers",
+  AccessControlAllowCredentials = "Access-Control-Allow-Credentials",
+  AccessControlExposeHeaders = "Access-Control-Expose-Headers",
+  AccessControlMaxAge = "Access-Control-Max-Age", // Security
+  StrictTransportSecurity = "Strict-Transport-Security",
+  XContentTypeOptions = "X-Content-Type-Options",
+  XXSSProtection = "X-XSS-Protection",
+  XFrameOptions = "X-Frame-Options",
+  ContentSecurityPolicy = "Content-Security-Policy",
+  ContentSecurityPolicyReportOnly = "Content-Security-Policy-Report-Only",
+  XCSRFToken = "X-CSRF-Token",
+  ReferrerPolicy = "Referrer-Policy"
+}
+
+/**
  * HTTP messages consist of requests from a client to a server and responses.
  * from a server to a client. This interface defines the methods common to each.
  *

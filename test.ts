9bc3de2281a4106c745899f25767eeefefb2857c
yasser <spookyframework@gmail.com>
2020-12-19 09:49:52 +0100

    chore: Remove test.ts file

--- test.ts
+++ test.ts
@@ -2,34 +2,46 @@ import {
   Application,
   HttpRequest,
   HttpResponse,
+  MiddlewareState,
   RequestMethod,
 } from "./lib/mod.ts";
 
-const TimeOut = async function(Request: HttpRequest, ResponseWriter: HttpResponse) {
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-      console.log("1")
-      resolve("1");
-    },1000);
-  });}
+const TimeOut = async function (
+  Request: HttpRequest,
+  ResponseWriter: HttpResponse,
+) {
+  console.log("Heee");
+  return MiddlewareState.Next;
+};
 
-const TimeOutFirst = async function(Request: HttpRequest, ResponseWriter: HttpResponse) {
+const TimeOutFirst = async function (
+  Request: HttpRequest,
+  ResponseWriter: HttpResponse,
+) {
   console.log("hello");
-}
+  return MiddlewareState.Next;
+};
 
-
-async function main(): Promise<void> {
+async function main(args: string[]): Promise<void> {
   const app = new Application();
-  const r = app.NewRoute({ maxRoutes:1 });
-  r.WithMethods(RequestMethod.POST)
-    .Path("/graphql")
-    .HandleFunc(async function (Request: HttpRequest, ResponseWriter: HttpResponse): Promise<any> {
-       console.log(await Request.GetBody());
-    });
+  const r = app.NewRoute({ maxRoutes: 1 });
+  r.WithMethods(RequestMethod.GET)
+    .WithMiddleware(TimeOut)
+    .WithName("")
+    .WithMiddleware(TimeOutFirst)
+    .Path(/graphql\/(?<id>[0-9]{1,})\/name\/(?<name>[a-z]+)/u)
+    .HandleFunc(
+      async function (
+        Request: HttpRequest,
+        ResponseWriter: HttpResponse,
+      ): Promise<any> {
+        console.log(await Request.GetParams());
+      },
+    );
 
   app.ListenAndServe({ port: 8080 });
 }
 
-main().then(() => {
+main(Deno.args).then(() => {
   console.log("Serveur listining");
 });

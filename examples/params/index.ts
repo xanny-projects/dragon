c1f91149c32b73f3f4363b0ebf36e6651ae9915f
yasser <spookyframework@gmail.com>
2021-01-01 22:54:12 +0100

    docs(examples/params): Add params examples

---
+++ examples/params/index.ts
@@ -0,0 +1,25 @@
+import {
+  Application,
+  HttpRequest,
+  HttpResponse,
+  RequestMethod,
+} from "../../lib/mod.ts";
+
+async function main(args: string[]): Promise<void> {
+  const app = new Application();
+
+  const r = app.routes({ maxRoutes: 1 });
+
+  r.Path(/user\/(?<id>[0-9]{1,})/u).withMethods(RequestMethod.GET).handleFunc(
+    async function (Request: HttpRequest, ResponseWriter: HttpResponse) {
+      const { id: userID } = Request.params();
+      ResponseWriter.withBody(`Hello ${userID}`).send();
+    },
+  );
+
+  app.listenAndServe({ port: 8080 });
+}
+
+await main(Deno.args);
+
+console.log("🦕 Xanny listining...");

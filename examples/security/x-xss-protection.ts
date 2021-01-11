b2e89f426ac1cf00905b926f44ed442821c0923a
getspooky <getspookydev@gmail.com>
2021-01-11 20:19:17 +0100

    chore(examples/security):  Add XSSProtectionMiddleware examples

---
+++ examples/security/x-xss-protection.ts
@@ -0,0 +1,25 @@
+import {
+  Application,
+  HttpRequest,
+  HttpResponse,
+  RequestMethod,
+  XSSProtectionMiddleware,
+} from "../../lib/mod.ts";
+
+async function main(args: string[]): Promise<void> {
+  const app = new Application();
+
+  const r = app.routes({ maxRoutes: 1 });
+
+  r.Path("/").withMethods(RequestMethod.GET).handleFunc(
+    async function (Request: HttpRequest, ResponseWriter: HttpResponse) {
+      ResponseWriter.withBody("Hello Dragon").send();
+    },
+  ).withMiddleware(XSSProtectionMiddleware);
+
+  app.listenAndServe({ port: 8080 });
+}
+
+await main(Deno.args);
+
+console.log("Dragon listining...");
